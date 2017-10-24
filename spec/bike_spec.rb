631ffc44fffa1cc2542bfc368693f878ec8f8d3e
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-24 16:14:15 +0100

    Test working passed

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,4 +5,8 @@ describe Bike do
   it "should respond to working?" do
   expect(Bike.new).to respond_to(:working?)
   end
+
+  it 'should return true' do
+  expect(Bike.new.working?).to eq(true)
+  end
 end
