5cfb41a6289ccdb97dab0b9c9b1d60a421ffb615
Andrew Davey <ajdavey8@gmail.com>
2017-10-24 15:53:41 +0100

    passed release a bike test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,10 @@
 require 'docking_station'
-
+require 'bike'
 describe DockingStation do
   it "should respond to release bike" do
     expect(DockingStation.new).to respond_to(:release_bike)
+  end
+  it "new instance should release a bike" do
+    expect(DockingStation.new.release_bike).to be_an_instance_of(Bike)
   end
 end
